b3a290169ad3497f7e0a1bca737c53d83af62771
RajaFaizan572 <Faizan-RM@ulster.ac.uk>
2025-08-26 10:23:25 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,20 @@
 <?php
-$host = 'clipwave.mysql.database.azure.com';
-$dbname = 'clipwave';
-$username = 'clipwave'; // Change this to your MySQL username
-$password = 'Mydata12'; // Change this to your MySQL password
+$host = "clipwave.mysql.database.azure.com";
+$user = "clipwave";  // use full format
+$password = "Mydata12";
+$database = "clipwave";
+$port = 3306;
 
-$conn = new mysqli($host, $username, $password, $dbname);
+// SSL certificate path (download BaltimoreCyberTrustRoot.crt.pem)
+$ssl_cert = __DIR__ . "C:/BaltimoreCyberTrustRoot.crt.pem";
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, $ssl_cert, NULL, NULL);
+mysqli_real_connect($conn, $host, $user, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
+
+if (mysqli_connect_errno($conn)) {
+    die("Failed to connect: " . mysqli_connect_error());
+} else {
+    echo "✅ Connected to Azure MySQL!";
 }
 ?>
